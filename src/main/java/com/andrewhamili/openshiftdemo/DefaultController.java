c446ccdc00ff834d8a037175f38d1e93cd3c67e0
andrewhamili <andrewhamilidevelopment@gmail.com>
2022-06-29 09:31:22 +0800

    changes

--- src/main/java/com/andrewhamili/openshiftdemo/DefaultController.java
+++ src/main/java/com/andrewhamili/openshiftdemo/DefaultController.java
@@ -1,6 +1,8 @@
 package com.andrewhamili.openshiftdemo;
 
 import com.andrewhamili.openshiftdemo.model.ApiResponse;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -11,13 +13,17 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping
 public class DefaultController {
 
+    private final Logger log = LoggerFactory.getLogger(this.getClass());
+
     @GetMapping("/")
     public ResponseEntity<ApiResponse> landing(){
+        log.info("landing");
         return new ResponseEntity<>(new ApiResponse("It works"), HttpStatus.OK);
     }
 
     @GetMapping("/test")
     public ResponseEntity<ApiResponse> test(){
+        log.info("test");
         return landing();
     }
 
